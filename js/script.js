5241e60715e77809a5dffd95df60f6bb9fe606d6
JustinMelore <88466579+JustinMelore@users.noreply.github.com>
2022-03-08 17:59:53 -0700

    Finished the about me section and created the carousel with some styling for the experience section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
 
+    function revealSection() {
+        $(this).addClass("revealed");
+        $(this).off("mouseenter",revealSection);
+    }
 
+    $("#aboutMe").on("mouseenter",revealSection);
+    $("#experience").on("mouseenter",revealSection);
 })
